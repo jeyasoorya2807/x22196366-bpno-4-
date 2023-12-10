d56b7104243c814e39262f187f0880a88409d83b
EC2 Default User <ec2-user@ip-172-31-10-158.eu-west-1.compute.internal>
2023-12-10 21:11:51 +0000

    8th commit

--- babyproducts/views.py
+++ babyproducts/views.py
@@ -23,12 +23,6 @@ def add_product(request):
     else:
         form = ProductForm()
     return render(request, 'babyproducts/add_product.html', {'form': form})
-    
-def add_to_cart(request id):
-    product = get_object_or_404(Product, pk=id)
-    # code to add the product to cart
-
-    return redirect('babyproducts:cart')
 
 @login_required
 def add_to_cart(request, product_id):
