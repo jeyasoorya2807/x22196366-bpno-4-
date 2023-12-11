f8a6724727d874f392c4c2a388b59bca3fa1e79e
EC2 Default User <ec2-user@ip-172-31-10-158.eu-west-1.compute.internal>
2023-12-11 13:45:58 +0000

    10th commit

--- babyproducts/views.py
+++ babyproducts/views.py
@@ -3,9 +3,6 @@ from django.contrib import messages
 from .models import Babyproduct, CartItem, Order
 from .forms import ProductForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 def index(request):
     return render(request, 'babyproducts/index.html')
@@ -25,9 +22,9 @@ def add_product(request):
     return render(request, 'babyproducts/add_product.html', {'form': form})
 
 def add_to_cart(request, product_id):
-    # if not request.user.is_authenticated:
-    #     messages.warning(request, 'Please log in to add items to your cart.')
-    #     return redirect('users:sign_in')
+    if not request.user.is_authenticated:
+        messages.warning(request, 'Please log in to add items to your cart.')
+        return redirect('sign_in')
 
     product = Babyproduct.objects.get(pk=product_id)
     cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
