58695f6c40783e8ad70ba8c9ef818c2fb7448d07
EC2 Default User <ec2-user@ip-172-31-10-158.eu-west-1.compute.internal>
2023-12-12 09:03:07 +0000

    sonar test

--- newone/settings.py
+++ newone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'am*hi8s04-^-kiv7z@^p^@3^cjo9jr!=ho_4&@5xhepmh@v5xk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
